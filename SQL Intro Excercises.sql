d8bdd7d23278b00e96450432210e42d58f8c8b7b
katherine Buendia <kbbuendia24@gmail.com>
2023-05-08 18:42:41 -0400

    SQL update

--- SQL Intro Excercises.sql
+++ SQL Intro Excercises.sql
@@ -37,12 +37,42 @@ from products;
 
 -- find all employees whose first name starts with the letter ‘j’
 
+select * 
+from employees
+where firstname like "j%";
+
 -- find all Macbooks
+
+select *
+from products 
+WHERE name like "%macbook%";
 
 -- find all products that are on sale
 
+select * 
+from products 
+where onsale = 1;
+
+
 -- find the average price of all products
 
--- find all Geek Squad employees who don't have a middle initial
+select	ave(price)
+from products ;
 
--- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
+
+-- find all Geek Squad employees who don't have a middle initial
+select * 
+from employees 
+where middleinitial  is null
+and title  like "%Geek Squad%";
+
+-- find all products from the products table whose stock level is in the range -- of 500 to 1200. 
+-- Order by Price from least to greatest. Hint: Use the between keyword
+
+select * 
+from products 
+where stocklevel 
+between 500 and 1200
+order by price ;
+
+
